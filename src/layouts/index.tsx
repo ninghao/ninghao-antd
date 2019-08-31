271641b805f6f00dee983b6c469a53cf6fb2a21a
wanghao <117663444@qq.com>
2019-08-31 23:41:01 +0800

    Layout

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,12 +1,28 @@
 import React from 'react';
+import { Layout, Button, Avatar, Icon } from 'antd';
 import styles from './index.css';
+
+const { Header, Content, Footer, Sider } = Layout;
 
 const BasicLayout: React.FC = props => {
   return (
-    <div className={styles.normal}>
-      <h1 className={styles.title}>Yay! Welcome to umi!</h1>
-      {props.children}
-    </div>
+    <Layout style={{ minHeight: '100vh' }}>
+      <Sider theme="light">
+        <div className={styles.logo}>Ninghao</div>
+      </Sider>
+      <Layout>
+        <Header className={styles.header}>
+          <Button type="link">
+            <Icon type="left" />
+            Go back
+          </Button>
+        </Header>
+        <Content>{props.children}</Content>
+        <Footer>
+          <Avatar className={styles.avatar}>N</Avatar>
+        </Footer>
+      </Layout>
+    </Layout>
   );
 };
 

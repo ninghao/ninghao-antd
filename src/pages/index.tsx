a03e6f5fc684539559863a931bae11f4f0017ba6
wanghao <117663444@qq.com>
2019-08-31 22:43:09 +0800

    Typography

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,19 @@
 import React from 'react';
+import { Typography } from 'antd';
 import styles from './index.css';
 import { formatMessage } from 'umi-plugin-locale';
+
+const { Title, Paragraph, Text } = Typography;
+
 export default function() {
   return (
     <div className={styles.normal}>
-      <div className={styles.welcome} />
-      <ul className={styles.list}>
-        <li>To get started, edit <code>src/pages/index.js</code> and save to reload.</li>
-        <li>
-          <a href="https://umijs.org/guide/getting-started.html">
-            {formatMessage({ id: 'index.start' })}
-          </a>
-        </li>
-      </ul>
+      <Typography>
+        <Title level={3}>hello ~</Title>
+        <Paragraph>
+          This is an <Text mark>Ant Design</Text> project.
+        </Paragraph>
+      </Typography>
     </div>
   );
 }

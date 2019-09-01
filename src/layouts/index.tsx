66ae6f9aabc601b3347ffa2b1e65f384756c216f
wanghao <117663444@qq.com>
2019-09-01 11:32:18 +0800

    Menu

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,14 +1,32 @@
 import React from 'react';
-import { Layout, Button, Avatar, Icon } from 'antd';
+import { Layout, Button, Avatar, Icon, Menu } from 'antd';
 import styles from './index.css';
 
 const { Header, Content, Footer, Sider } = Layout;
+const { SubMenu } = Menu;
 
 const BasicLayout: React.FC = props => {
   return (
     <Layout style={{ minHeight: '100vh' }}>
       <Sider theme="light">
         <div className={styles.logo}>Ninghao</div>
+        <Menu mode="inline">
+          <Menu.Item>
+            <Icon type="dashboard" />
+            <span>Dashboard</span>
+          </Menu.Item>
+          <SubMenu
+            title={
+              <span>
+                <Icon type="appstore" />
+                <span>Discover</span>
+              </span>
+            }
+          >
+            <Menu.Item>Popular</Menu.Item>
+            <Menu.Item>Fresh</Menu.Item>
+          </SubMenu>
+        </Menu>
       </Sider>
       <Layout>
         <Header className={styles.header}>
